db54edab3d16633854600a1c9f38a47416295e08
Brandon Stultz <brandon.stultz@snhu.edu>
2022-05-22 17:10:33 -0700

    Changed routing of travel page. Now uses handlebars partials and utilizes JSOn data instead of hardcodded HTML code.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
+
+const blogs = JSON.parse(fs.readFileSync('./data/blogs.json', 'utf8'));
+
+
 /*GET homepage*/
 const index = (req, res) => {
-    res.render('index', { title: 'Travlr Getaways' });
+    res.render('index', { title: 'Travlr Getaways', blogs});
 };
 
 module.exports = {
     index
-}
+};
